5f934209c23a818865102683e9f825a2475f452a
jlsh123 <140146393+jlsh123@users.noreply.github.com>
2023-07-21 15:22:02 +0800

    Create Auto.java
    auto mode example

---
+++ Auto.java
@@ -0,0 +1,22 @@
+//this is an example code for auto mode (note: it needs a hwMap to work)
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+
+@Autonomous(name = "backup_autoL1", group = "final")
+public class Auto extends LinearOpMode {
+    //define variables here
+    hwMap robot = new hwMap();
+
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+        waitForStart();
+        //auto code here
+        robot.EncoderForward_CM(10,1);
+    }
+}
